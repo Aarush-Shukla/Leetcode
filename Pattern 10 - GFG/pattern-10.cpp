dc0df6ba7595a0586f146c293589c64ce5acdb95
annihilator_as <98807835+Aarush-Shukla@users.noreply.github.com>
2023-01-04 11:48:55 +0530

    Added solution - LeetHub

---
+++ Pattern 10 - GFG/pattern-10.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//Back-end complete function Template for C++
+class Solution{
+public:
+    void printTriangle(int n) {
+      
+        for(int i=1;i<=n;i++){
+          
+            for(int j=1;j<=i;j++){
+                cout<<"* ";
+            }
+            cout<<endl;
+        }
+        for(int i=n-1;i>0;i--){
+          
+            for(int j=i-1;j>=0;j--){
+                cout<<"* ";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
