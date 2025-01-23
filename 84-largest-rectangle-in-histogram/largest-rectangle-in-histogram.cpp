d4dab75fdb429b5d29165d870f395ad365f3cb40
Aarush-Shukla <98807835+Aarush-Shukla@users.noreply.github.com>
2025-01-23 19:22:00 +0530

    Time: 15 ms (88.09%) | Memory: 85.9 MB (33.36%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -26,6 +26,32 @@ public:
         return ans;
     }
 
+    vector<int> prevGreaterElement(vector<int>& heights, int n) {
+    stack<int> st;
+    vector<int> ans(n);
+    for (int i = 0; i < n; i++) {
+        while (!st.empty() && heights[i] >= heights[st.top()]) {
+            st.pop();
+        }
+        ans[i] = st.empty() ? -1 : st.top();
+        st.push(i);
+    }
+    return ans;
+}
+
+vector<int> nextGreaterElement(vector<int>& heights, int n) {
+    stack<int> st;
+    vector<int> ans(n);
+    for (int i = n - 1; i >= 0; i--) {
+        while (!st.empty() && heights[i] >= heights[st.top()]) {
+            st.pop();
+        }
+        ans[i] = st.empty() ? n : st.top();
+        st.push(i);
+    }
+    return ans;
+}
+
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
         if (n == 0) return 0; // Handle empty input
