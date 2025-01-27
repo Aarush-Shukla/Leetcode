28001ac6a1d8d3fb51a3bfb4dea7e834b7658938
Aarush-Shukla <98807835+Aarush-Shukla@users.noreply.github.com>
2025-01-27 08:26:56 +0530

    Time: 19 ms (22.01%) | Memory: 18.2 MB (46.13%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    int solve(int ind, int buy, int cap, int n, vector<int>& arr, vector<vector<vector<int>>>& dp) {
-        if (ind == n || cap == 0) return 0;
-        if (dp[ind][buy][cap] != -1) return dp[ind][buy][cap];
+    int solve(int ind, int buy, int k, int n, vector<int>& arr,vector<vector<vector<int>>>& dp) {
+        if (ind == n || k == 0)
+            return 0;
+        if (dp[ind][buy][k] != -1)
+            return dp[ind][buy][k];
 
         int profit = 0;
         if (buy == 1) {
-            int take = -arr[ind] + solve(ind + 1, 0, cap, n, arr, dp);
-            int not_take = solve(ind + 1, 1, cap, n, arr, dp);
+            int take = -arr[ind] + solve(ind + 1, 0, k, n, arr, dp);
+            int not_take = solve(ind + 1, 1, k, n, arr, dp);
             profit = max(take, not_take);
         } else {
-            int take = arr[ind] + solve(ind + 1, 1, cap - 1, n, arr, dp);
-            int not_take = solve(ind + 1, 0, cap, n, arr, dp);
+            int take = arr[ind] + solve(ind + 1, 1, k - 1, n, arr, dp);
+            int not_take = solve(ind + 1, 0, k, n, arr, dp);
             profit = max(take, not_take);
         }
 
-        return dp[ind][buy][cap] = profit;
+        return dp[ind][buy][k] = profit;
     }
 
-    int maxProfit(int cap, vector<int>& prices) {
+    int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(cap + 1, -1)));
-        return solve(0, 1, cap, n, prices, dp);
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k + 1, -1)));
+        return solve(0, 1, k, n, prices, dp);
     }
 };
