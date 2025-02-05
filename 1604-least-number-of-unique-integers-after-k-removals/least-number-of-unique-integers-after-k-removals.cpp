79b9baed393cedae01929c17ed3d43c64382b69e
Aarush-Shukla <98807835+Aarush-Shukla@users.noreply.github.com>
2025-02-05 17:20:34 +0530

    Time: 138 ms (31.82%) | Memory: 68.9 MB (32.82%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
-  unordered_map<int, int> mpp;
-        for (auto it : arr) {
+        unordered_map<int,int> mpp;
+        for(auto it: arr){
             mpp[it]++;
         }
-
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        for (auto it : mpp) {
-            pq.push({it.second, it.first});
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        for(auto it: mpp){
+            pq.push({it.second,it.first});
         }
-
-        while (k > 0 && !pq.empty()) {
-            auto [freq, num] = pq.top();  // Extract smallest frequency element
-            pq.pop();
-            k -= freq;  // Use k to remove this entire frequency
-
-            if (k < 0) {  // If k goes negative, it means we can't remove this number entirely
-                pq.push({-k, num});  // Push back with the remaining frequency
-                break;
-            }
+        while(k>0){
+            int temp=pq.top().second;
+            int freq=mpp[temp];
+            
+                freq--;
+                k--;
+            
+            if(freq==0)pq.pop();
+            
+                mpp[temp]=freq;
+            
         }
-        return pq.size();
+        int ans=0;
+          for(auto it: mpp){
+            if(mpp[it.first]!=0)ans++;
+        }
+        return ans;
     }
 };
