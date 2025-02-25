952f3cff3bbf7c886e4e96015e0e12b12594c32a
Aarush-Shukla <98807835+Aarush-Shukla@users.noreply.github.com>
2025-02-26 00:52:47 +0530

    Time: 0 ms (100.00%) | Memory: 32.1 MB (70.06%) - LeetSync

--- 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.cpp
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.cpp
@@ -20,35 +20,35 @@
  * };
  */
 class Solution {
-private:
-    bool check(ListNode *head, TreeNode* root){
+public:
+
+    bool check(ListNode* head, TreeNode* root){
         if(!root)return false;
-        if(head->val!=root->val){
+        if(head->val != root->val){
             return false;
         }
-        if(head->next==NULL){
-            return true;
-        }
+        if(head->next==NULL)return true;
+
         bool left=false;
         bool right=false;
-        if(root->left&&head->next&&root->left->val==head->next->val){
+        if(root->left && head->next && root->left->val == head->next->val){
             left=check(head->next,root->left);
         }
-        if(root->right&&head->next&&root->right->val==head->next->val){
+        if(root->right && head->next && root->right->val == head->next->val){
             right=check(head->next,root->right);
         }
-        return left|right;
+
+        return left || right;
     }
-public:
+
     bool isSubPath(ListNode* head, TreeNode* root) {
-        if(!root){
-            return false;
-        }
-        if(root->val==head->val){
+        if (!root)return false;
+
+        if(head->val == root->val){
             if(check(head,root)){
                 return true;
             }
         }
-        return isSubPath(head,root->left)|isSubPath(head,root->right);
+        return isSubPath(head,root->left) || isSubPath(head,root->right);
     }
 };
