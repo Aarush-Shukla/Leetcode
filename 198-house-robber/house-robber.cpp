28ce9162b44f05f8049318d566dadc5e760f624f
Aarush-Shukla <98807835+Aarush-Shukla@users.noreply.github.com>
2025-03-09 23:31:09 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (5.90%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-      vector<int> dp(n+1,-1);
+      vector<int> dp(n,-1);
         dp[0]=nums[0];
         for(int i=1;i<nums.size();i++){
             int pick=nums[i];
